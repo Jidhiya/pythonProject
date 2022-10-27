9fca988244e4a6b47426c8787a69da485b12d95e
Jidhiya Vijayan <jidhiya@outlook.com>
2022-10-27 13:57:57 -0400

    Next Commit

--- demo.py
+++ demo.py
@@ -12,3 +12,28 @@ fnam = input("May I have your first name, please? ")
 lnam = input("May I have your last name, please? ")
 print("Thank you.")
 print("\nYour name is " + fnam + " " + lnam + ".")
+
+
+# input a float value for variable a here
+a = 9.0
+# input a float value for variable b here
+b = 8.0
+
+# output the result of addition here
+add_result = a+b
+print("Addition result is :" , add_result)
+
+# output the result of subtraction here
+sub_result = a-b
+print("Subtraction result is :" , sub_result)
+
+# output the result of multiplication here
+
+multiply_result = a+b
+print("Multipication result is :" , multiply_result)
+
+# output the result of division here
+div_result = a+b
+print("Division result is :" , div_result)
+
+print("\nThat's all, folks!")
