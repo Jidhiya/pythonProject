043863e73ce07f649a5ec19eeee403c91db9154e
Jidhiya Vijayan <jidhiya@outlook.com>
2022-10-27 13:47:40 -0400

    Second Commit

--- demo.py
+++ demo.py
@@ -7,3 +7,8 @@ leg_a = float(input("Input first leg length: "))
 leg_b = float(input("Input second leg length: "))
 hypo = (leg_a**2 + leg_b**2) ** .5
 print("Hypotenuse length is", hypo)
+
+fnam = input("May I have your first name, please? ")
+lnam = input("May I have your last name, please? ")
+print("Thank you.")
+print("\nYour name is " + fnam + " " + lnam + ".")
