536b4d0634a436f1ab42cffaf76239549a67c501
Jidhiya Vijayan <jidhiya@outlook.com>
2022-10-27 13:41:46 -0400

    Initial Commit
    second commit

--- demo.py
+++ demo.py
@@ -1,3 +1,9 @@
 print("Tell me anything")
 anything = input()
 print("Hmmm", anything, "Really??")
+
+
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("Input second leg length: "))
+hypo = (leg_a**2 + leg_b**2) ** .5
+print("Hypotenuse length is", hypo)
