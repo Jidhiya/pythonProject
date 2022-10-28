9324a9ca81ca310afba23495882a9416a34bd9ac
Jidhiya Vijayan <jidhiya@outlook.com>
2022-10-28 13:48:40 -0400

    Python Certification Prep Next Commit

--- pythonCertModule2.py
+++ pythonCertModule2.py
@@ -1,3 +1,14 @@
 x = float(input("Enter value for x: "))
 y = (1/(x+1/(x+1/(x+1/x))))
 print("y =", y)
+
+
+
+hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+
+hour = (hour + dura//60 + (mins+ dura%60)//60)%24
+mins = (mins+ dura%60)%60
+print(hour,":",mins,sep="")
+
